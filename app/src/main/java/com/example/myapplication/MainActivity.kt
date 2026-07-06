cf3b33401661c0bc3bef8a89787e4a9e5578be9d
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2026-07-06 14:19:00 +0800

    composable edit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +39,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+            .padding(all = 16.dp)
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.onSurfaceVariant),
+        Arrangement.Center
+    ) {
+        Text(
+            text = "Hello $name",
+            textAlign = TextAlign.Center,
+            modifier = modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
